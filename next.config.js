5435da97d315de7c83cf7676b58beb8152f088ed
khanshujaath007 <khanshujaath007@.com>
2024-08-10 17:08:41 +0530

    add changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-// await import("./src/env.js"); // Comment this line out if not needed
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
 import WithPWA from "next-pwa";
 
 const withPWA = WithPWA({
@@ -15,13 +19,17 @@ const withPWA = WithPWA({
 // @ts-ignore
 const config = withPWA({
   reactStrictMode: true,
-  
-  output: 'export', // Ensure this line is here for static export
-  
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+
 });
 
 export default config;
